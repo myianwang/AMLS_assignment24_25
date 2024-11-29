2b243ec1a1f532becef18f826e838a5c74b43f1e
myianwang <myianwang@gmail.com>
2024-11-29 21:54:11 +0000

    Update the data import in Task B

--- AMLS_24_25_21023692/B/TaskB.py
+++ AMLS_24_25_21023692/B/TaskB.py
@@ -1,9 +1,9 @@
 # Python Script for Task B
 # Import the data from the medmnist for task B
-from medmnist import ChestMNIST
-train_data = ChestMNIST(split='train', download=True, size=64)
-val_data = ChestMNIST(split='val', download=True, size=64)
-test_data = ChestMNIST(split='test', download=True, size=64)
+from medmnist import BloodMNIST
+train_data = BloodMNIST(split='train', download=True, size=64)
+val_data = BloodMNIST(split='val', download=True, size=64)
+test_data = BloodMNIST(split='test', download=True, size=64)
 print(test_data)
 print(val_data)
 print(train_data)
