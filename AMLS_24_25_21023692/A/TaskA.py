eba8cbf6e830af86cb042d08a5d65543a94e514d
myianwang <myianwang@gmail.com>
2024-12-10 21:03:40 +0000

    Add possible model choice for Task A

--- AMLS_24_25_21023692/A/TaskA.py
+++ AMLS_24_25_21023692/A/TaskA.py
@@ -1,9 +1,12 @@
 # Python Script for Task A
 # Import the data from the medmnist for task A
 from medmnist import BreastMNIST
+
 train_data = BreastMNIST(split='train', download=True, size=64)
 val_data = BreastMNIST(split='val', download=True, size=64)
 test_data = BreastMNIST(split='test', download=True, size=64)
 print(test_data)
 print(val_data)
 print(train_data)
+
+# CNN Model can be used for Task A
