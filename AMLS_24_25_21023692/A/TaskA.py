96815f11e0aeaaae467eb7cd7c0e109394619076
myianwang <myianwang@gmail.com>
2024-12-14 08:42:42 +0000

    function to load data for task A

--- AMLS_24_25_21023692/A/TaskA.py
+++ AMLS_24_25_21023692/A/TaskA.py
@@ -1,12 +1,20 @@
 # Python Script for Task A
 # Import the data from the medmnist for task A
-from medmnist import BreastMNIST
+import medmnist
+from medmnist import INFO
 
-train_data = BreastMNIST(split='train', download=True, size=64)
-val_data = BreastMNIST(split='val', download=True, size=64)
-test_data = BreastMNIST(split='test', download=True, size=64)
-print(test_data)
-print(val_data)
-print(train_data)
+
+# Load BreastMNIST dataset
+def load_data():
+    info = INFO['breastmnist']
+    DataClass = getattr(medmnist, info['python_class'])
+
+    train_data = DataClass(split='train', download=True)
+    val_data = DataClass(split='val', download=True)
+    test_data = DataClass(split='test', download=True)
+
+    print(test_data)
+    print(val_data)
+    print(train_data)
 
 # CNN Model can be used for Task A
