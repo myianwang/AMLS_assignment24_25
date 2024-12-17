3a6769200fe9c31811d1c9e49032ee435fa32a4d
myianwang <myianwang@gmail.com>
2024-12-17 05:26:45 +0000

    complete load data function

--- AMLS_24_25_21023692/A/TaskA.py
+++ AMLS_24_25_21023692/A/TaskA.py
@@ -1,6 +1,7 @@
 # Python Script for Task A
 # Import the data from the medmnist for task A
 from medmnist import BreastMNIST
+from tensorflow.keras.utils import to_categorical
 
 
 # Load BreastMNIST dataset
@@ -13,9 +14,14 @@ def load_data():
     x_val, y_val = val_data.imgs, val_data.labels
     x_test, y_test = test_data.imgs, test_data.labels
 
+    y_train = to_categorical(y_train, num_classes=2)
+    y_test = to_categorical(y_test, num_classes=2)
+
     print(test_data)
     print(val_data)
     print(train_data)
+
+    return (x_train, y_train), (x_test, y_test)
 
 
 load_data()
--- AMLS_24_25_21023692/B/TaskB.py
+++ AMLS_24_25_21023692/B/TaskB.py
@@ -1,6 +1,7 @@
 # Python Script for Task B
 # Import the data from the medmnist for task B
 from medmnist import BloodMNIST
+from tensorflow.keras.utils import to_categorical
 
 
 # Load BloodMNIST dataset
@@ -13,9 +14,14 @@ def load_data():
     x_val, y_val = val_data.imgs, val_data.labels
     x_test, y_test = test_data.imgs, test_data.labels
 
+    y_train = to_categorical(y_train, num_classes=8)
+    y_test = to_categorical(y_test, num_classes=8)
+
     print(test_data)
     print(val_data)
     print(train_data)
+
+    return (x_train, y_train), (x_test, y_test)
 
 
 load_data()
