22cc67e7d0f231385687a1af3712d57c6903a1a3
myianwang <myianwang@gmail.com>
2024-12-21 11:57:30 +0000

    Update load function for TaskB.py

--- AMLS_24_25_21023692/B/TaskB.py
+++ AMLS_24_25_21023692/B/TaskB.py
@@ -1,29 +1,36 @@
-# Python Script for Task B
-# Import the data from the medmnist for task B
-from medmnist import BloodMNIST
+import matplotlib.pyplot as plt
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
 from tensorflow.keras.utils import to_categorical
+from medmnist import BloodMNIST
 
 
-# Load BloodMNIST dataset
 def load_data():
-    train_data = BloodMNIST(split='train', download=True, size=224)
-    val_data = BloodMNIST(split='val', download=True, size=224)
-    test_data = BloodMNIST(split='test', download=True, size=224)
+    """
+    Load the BloodMNIST dataset and preprocess the data.
+    :return x_train: training images
+    :return y_train: training labels
+    :return x_val: validation images
+    :return y_val: validation labels
+    :return x_test: test images
+    :return y_test: test labels
+    """
 
+    # Load BloodMNIST training, validation, and test datasets
+    train_data = BloodMNIST(split='train', download=True, size=28)
+    val_data = BloodMNIST(split='val', download=True, size=28)
+    test_data = BloodMNIST(split='test', download=True, size=28)
+
+    # Get the images and labels
     x_train, y_train = train_data.imgs, train_data.labels
     x_val, y_val = val_data.imgs, val_data.labels
     x_test, y_test = test_data.imgs, test_data.labels
 
+    # One-hot encode the labels
     y_train = to_categorical(y_train, num_classes=8)
+    y_val = to_categorical(y_val, num_classes=8)
     y_test = to_categorical(y_test, num_classes=8)
 
-    print(test_data)
-    print(val_data)
-    print(train_data)
-
-    return (x_train, y_train), (x_test, y_test)
-
-
-load_data()
+    return x_train, y_train, x_val, y_val, x_test, y_test
 
 # CNN Model can be used for Task B
