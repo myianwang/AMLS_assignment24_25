bd550aa8740c36d50b5020a5a2a7471b72981d31
myianwang <myianwang@gmail.com>
2025-01-08 04:00:46 +0000

    import packages for TaskBSVM.py

--- AMLS_24_25_21023692/B/TaskBSVM.py
+++ AMLS_24_25_21023692/B/TaskBSVM.py
@@ -1,1 +1,7 @@
-# SVM for Task B
+import matplotlib.pyplot as plt
+from medmnist import BloodMNIST
+from sklearn.svm import SVC
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import ConfusionMatrixDisplay
