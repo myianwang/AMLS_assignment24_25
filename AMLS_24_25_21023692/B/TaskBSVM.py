7fb571904f3c24eb5b98966f278a4c609896bcfb
myianwang <myianwang@gmail.com>
2025-01-12 00:33:20 +0000

    train and evaluate function for TaskBSVM.py

--- AMLS_24_25_21023692/B/TaskBSVM.py
+++ AMLS_24_25_21023692/B/TaskBSVM.py
@@ -54,3 +54,46 @@ def preprocess_data(x_train, x_val, x_test):
     x_test_prepared = scaler.transform(x_test_prepared)
 
     return x_train_prepared, x_val_prepared, x_test_prepared
+
+
+def train_svm_and_evaluate(x_train, y_train, x_val, y_val):
+    """
+    Train a Support Vector Machine (SVM) model and evaluate it on the validation set.
+    :param x_train: training images
+    :param y_train: training labels
+    :param x_val: validation images
+    :param y_val: validation labels
+    :return best_svm: best SVM model
+    """
+
+    # Grid Search to find the best Hyperparameters
+    param_grid = {
+        'C': [0.1, 0.5, 0.8, 1, 2, 3, 4, 5, 7, 10, 20, 30],
+        'gamma': ['scale', 0.01, 0.05, 0.1],
+        'kernel': ['rbf', 'linear']
+    }
+
+    svm = SVC()
+    grid_search = GridSearchCV(svm, param_grid, cv=3, scoring='accuracy')
+    grid_search.fit(x_train, y_train)
+
+    print("Best Parameters:", grid_search.best_params_)
+
+    # Train the best model with the training set
+    best_svm = grid_search.best_estimator_
+    best_svm.fit(x_train, y_train)
+
+    # Evaluate the model with validation set
+    val_preds = best_svm.predict(x_val)
+    val_accuracy = accuracy_score(y_val, val_preds)
+
+    print("\nValidation Accuracy:", val_accuracy)
+    print("\nClassification Report:\n", classification_report(y_val, val_preds))
+
+    # Plot confusion matrix for validation set
+    cm = confusion_matrix(y_val, val_preds)
+    ConfusionMatrixDisplay(cm, display_labels=[str(i) for i in range(8)]).plot()
+    plt.title("Task B (SVM):Validation Confusion Matrix")
+    plt.show()
+
+    return best_svm
